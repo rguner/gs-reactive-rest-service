468ca19fdeac6443cebcbdf5444dbee2adcdaec3
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-03-02 14:22:15 +0300

    rest ile deneme

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,14 +3,26 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.IntStream;
+
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
 
-		GreetingWebClient gwc = new GreetingWebClient();
-		System.out.println(gwc.getResult());
 
-	}
+        GreetingWebClient gwc = new GreetingWebClient();
+        IntStream.range(1, 2).forEach(i -> {
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(gwc.getResult());
+                }
+        );
+
+    }
 }
